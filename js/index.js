5b64cb97f8a513028b06db220032abc02a93eca6
schazbot <charlotte.pearce1984@gmail.com>
2019-11-26 17:49:54 +0000

    review

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 //api
-
 const apiHeaders = {
   "Content-Type": "application/json",
   Accept: "application/json"
@@ -19,72 +18,83 @@ const patch = (url, id, bookData) => {
 
 const API = { get, patch };
 
-//consts
-const booksUrl = "http://localhost:3000/books/";
+//const
+
+const BOOKS_URL = "http://localhost:3000/books/";
 const listPanel = document.querySelector("#list");
 const showPanel = document.querySelector("#show-panel");
 const currentUser = { id: 1, username: "pouros" };
 
-//functions
+//functions - don't forget to call the master function
 
 const getAllBooks = () => {
-  API.get(booksUrl).then(books => books.forEach(book => makeBookPreview(book)));
+  API.get(BOOKS_URL).then(books =>
+    books.forEach(book => makeBookPreview(book))
+  );
 };
 
 const makeBookPreview = book => {
-  let li = document.createElement("li");
+  const li = document.createElement("li");
   li.innerText = book.title;
-  listPanel.append(li);
   li.addEventListener("click", () => makeBookDetail(book));
+
+  listPanel.append(li);
 };
 
 const makeBookDetail = book => {
   while (showPanel.firstChild) showPanel.removeChild(showPanel.firstChild);
-  const h1 = document.createElement("h1");
-  h1.innerText = book.title;
 
-  const img = document.createElement("img");
-  img.src = book.img_url;
+  const h2 = document.createElement("h2");
+  h2.innerText = book.title;
 
   const p = document.createElement("p");
   p.innerText = book.description;
+
+  const img = document.createElement("img");
+  img.src = book.img_url;
 
   const ul = document.createElement("ul");
   ul.id = "users-ul";
 
   book.users.forEach(bkUsr => {
-    let li = document.createElement("li");
-    li.id = `user-${bkUsr.id}`;
+    const li = document.createElement("li");
     li.innerText = bkUsr.username;
+    li.id = `user-${bkUsr.id}`;
     ul.append(li);
   });
 
-  const btn = document.createElement("button");
-  btn.innerText = "Like Me";
-  btn.addEventListener("click", () => handleButtonClick(book));
+  const button = document.createElement("button");
+  button.innerText = "Like Me";
+  button.addEventListener("click", () => handleButtonClick(book, ul));
 
-  showPanel.append(h1, img, p, ul, btn);
+  showPanel.append(h2, img, p, ul, button);
 };
 
-const handleButtonClick = book => {
+const handleButtonClick = (book, ul) => {
   if (!hasUserReadThisBook(book)) {
     book.users.push(currentUser);
-    API.patch(booksUrl, book.id, book);
-    const userLi = document.createElement("li");
-    userLi.innerText = currentUser.username;
-    userLi.id = `user-${currentUser.id}`;
-    const ul = document.querySelector("#users-ul");
-    ul.append(userLi);
+    API.patch(BOOKS_URL, book.id, book).then(makeLi(ul));
   } else {
-      book.users = book.users.filter(bookUsr => bookUsr.id !== currentUser.id)
-      API.patch(booksUrl, book.id, book)
-      foundLi = document.querySelector(`#user-${currentUser.id}`)
-      foundLi.remove()
+    book.users = book.users.filter(bookUser => bookUser.id !== currentUser.id);
+    API.patch(BOOKS_URL, book.id, book).then(removeLi)
+    
   }
 };
 
+const makeLi = (ul) => {
+  const li = document.createElement("li");
+  li.innerText = currentUser.username;
+  li.id = `user-${currentUser.id}`;
+  ul.append(li);
+};
+
+const removeLi = () => { 
+  const foundLi = document.querySelector(`#user-${currentUser.id}`);
+  foundLi.remove();
+}
+
 const hasUserReadThisBook = book => {
-  return book.users.find(bkUser => bkUser.id === currentUser.id);
+  return book.users.find(bookUser => bookUser.id === currentUser.id);
 };
 
 getAllBooks();
